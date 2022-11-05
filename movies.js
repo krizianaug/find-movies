bda9ae43ee96c7cb4a762224fb4f198a5e945251
Krizian <krzaugusto@gmail.com>
2022-11-05 09:22:58 -0400

    Homepage changed

--- movies.js
+++ movies.js
@@ -1,15 +1,37 @@
+let movies;
+
 const moviesWrapper = document.querySelector(".movies__list")
 const searchTerm = localStorage.getItem("searchTerm")
 
-console.log(searchTerm)
 
-async function renderMovies() {
+
+async function renderMovies(filter) {
     
     const movies = await fetch(`https://www.omdbapi.com/?apikey=788103b6&type=movie&s=${searchTerm}`)
     const moviesData = await movies.json();
     console.log(moviesData)
     moviesWrapper.innerHTML = moviesData.Search.map((movie) => moviesHTML(movie)).join("");
 
+    if (filter === 'DATE_LOW_TO_HIGH') {
+        movies.Search.sort((a, b) => (a.Year - b.Year));
+      }
+      else if (filter === 'DATE_HIGH_TO_LOW') {
+        movies.Search.sort((a, b) => (b.Year - a.Year));
+      }
+      else if (filter === 'TITLE_LOW_TO_HIGH') {
+        movies.Search.sort((a, b) => a.Title - b.Title);
+    
+      }
+      else if (filter === 'TITLE_HIGH_TO_LOW') {
+        movies.Search.sort((a, b) => b.Title - a.Title);
+    
+      }
+
+}
+
+function filterMovies(event){
+    
+    renderMovies(event.target.value)
 }
 
 
